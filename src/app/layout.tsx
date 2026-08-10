2736c3e6bed22f3bd44021b0a4c7dc205ef44967
Salvatore Bonavita <salvo.bonavita9808@gmail.com>
2026-08-10 19:06:46 +0200

    aggiornamento

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { DM_Sans } from "next/font/google";
+import Script from "next/script";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { CartDrawer } from "@/components/CartDrawer";
@@ -43,7 +44,26 @@ export default function RootLayout({
       lang="it"
       className={`${dmSans.variable} ${cooperBlack.variable} h-full`}
     >
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N4MCZ4R2');
+          `}
+        </Script>
+      </head>
       <body className="flex min-h-full flex-col antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N4MCZ4R2"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <CartProvider>
           <Header />
           <main>{children}</main>
